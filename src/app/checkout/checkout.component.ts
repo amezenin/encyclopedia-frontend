0f2f31efd5990a9f00ba539a05052640f231d57f
anton <anton.mezhenin@gmail.com>
2022-07-27 13:35:30 +0300

    Validations

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms'
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-checkout',
@@ -11,21 +11,33 @@ export class CheckoutComponent implements OnInit {
   checkoutForm: FormGroup; // like in html [formGroup] name
 
   constructor(private formBuilder: FormBuilder) {
+    /*
     this.checkoutForm = formBuilder.group({ // building form by FormBuilder
     email: new FormControl(),  // creating a group of form elements, same like in template formControlName = "email"
     status: new FormControl(),
     role: new FormControl(),
     content: new FormControl(),
     terms: new FormControl()
-    })
+    })*/
+
+    //Validation example
+    //in tamplate do deactive post button with: [disabled]="!checkoutForm.valid"
+    //if not all elements in form is filled - form is invalid and button not active
+    this.checkoutForm = formBuilder.group({ // building form by FormBuilder
+      email: ['', [Validators.required, Validators.email, Validators.minLength(5), Validators.maxLength(30)]],  
+      status: ['', Validators.required],
+      role: ['', Validators.required],
+      content: ['', Validators.required],
+      terms: ['', Validators.requiredTrue] //for checkbox requiredTrue
+      })
   }
 
   ngOnInit(): void {
   }
 
   postData(){
-    console.log('FormGroup object ' + this.checkoutForm);
-    console.log('values ' + this.checkoutForm.value); // can get values, valides etc...
+    console.log(this.checkoutForm);
+    console.log(this.checkoutForm.value); // can get values, valides etc...
     console.log('only email ' + this.checkoutForm.value.email);
   }
 
